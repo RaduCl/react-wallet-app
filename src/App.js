8d3874970a3ae9ca30df15d12e8e6fc9f9e8d4ea
RaduCl <raduclitan@gmail.com>
2016-10-15 22:26:31 +0300

    basic css elements sketching

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import minusIcon from './minus.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <img className="logo" src="/wallet-logo.png"></img>
           <nav>
             <a href="/">HOME</a>
             <a href="/">RESET WALLET</a>
-            <a href="/">SOURCE</a>
+            <a href="https://github.com/RaduCl/react-wallet-app" target="blank">SOURCE</a>
           </nav>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="app-container" >
+
+          <div className="app-body" >
+            <div className="wallet-controls" >
+              <button>Add</button>
+              <input
+                type="text"
+                placeholder="Enter amount here"
+              ></input>
+              <button>Retrieve</button>
+            </div>
+
+            <div className="wallet-entries" >
+              <div className="transaction-entry" >
+                <div className="transaction-icon-retrieve" ></div>
+                <span>$ 250</span>
+                <span>25 Sep 2016 15:04</span>
+              </div>
+
+              <div className="transaction-entry" >
+                <div className="transaction-icon-add" ></div>
+                <span>$ 500</span>
+                <span>22 Sep 2016 17:54</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
